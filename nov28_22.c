81af4b911a7d3d6eab9a3e4a2c7562e4ff7b6ae4
Sudipta Maity <sudiptamaity0492@gmail.com>
2022-11-29 10:45:53 +0530

    Update nov28_22.c

--- nov28_22.c
+++ nov28_22.c
@@ -1,6 +1,8 @@
 //Date:28.11.22
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
 void twofork()
 {
 	pid_t pid;
@@ -30,30 +32,32 @@ void q1()
 	}
 	printf(" Process id %ld Parent process id %ld \n",(long)getpid(),(long)getppid());
 }
-void q2()
+void q2(int n)
 {
 	pid_t pid;
-	int i,n;
-	if(argc<2)
-	{
-		printf("Usage : process input");
-		exit(0);
-	}
-	n=atoi(argv[1]);
+	int i;
 	for(i=0;i<n;i++)
 	{
-		pid fork();
+		pid=fork();
 		if(pid==0)
 			break;
 	}
+	printf(" Process id %ld Parent process id %ld \n",(long)getpid(),(long)getppid());
 }
-int main()
+	
+int main(int argc,char* argv[])
 {
 	//Draw the process diagram for the following functions call.
 	//twofork();
 	//threefork();
 	//write a function to call three forks. The following fork should only be called by the parent,and not the child created by the root parent.
-	q1();
+	//q1();
 	//write a function to generate an error. An error occurs when a parent gets terminated before the child.
-	q2();
+	if(argc<2)
+	{
+		printf("Usage : process input");
+		return 0;
+	}
+	int n=atoi(argv[1]);
+	q2(n);
 }
